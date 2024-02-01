f6b941142fb61608f3f7d61a210a80048da5cefd
Kanish Chheda <kanishchheda@gmail.com>
2024-02-01 22:11:33 +0530

    Board List

--- app/(dashboard)/_components/BoardList.tsx
+++ app/(dashboard)/_components/BoardList.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
 import EmptyBoards from "./emptyStates/EmptyBoards";
 import EmptyFavorites from "./emptyStates/EmptyFavorites";
 import EmptySearch from "./emptyStates/EmptySearch";
+import BoardCard from "./board-card";
+import NewBoardButton from "./board-card/NewBoardButton";
 
 interface BoardListProps {
   orgId: string;
@@ -13,7 +18,24 @@ interface BoardListProps {
 }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = [];
+  const data = useQuery(api.boards.get, { orgId });
+
+  if (data === undefined) {
+    return (
+      <div className="">
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite Boards" : "Team Boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 ms:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton disabled orgId={orgId} />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+        </div>
+      </div>
+    );
+  }
 
   if (!data?.length && query.search) {
     return <EmptySearch />;
@@ -25,7 +47,29 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
 
   if (!data?.length) return <EmptyBoards />;
 
-  return <div>BoardList</div>;
+  return (
+    <div className="">
+      <h2 className="text-3xl">
+        {query.favorites ? "Favorite Boards" : "Team Boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 ms:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default BoardList;
---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { formatDistanceToNow } from "date-fns";
+import Link from "next/link";
+import Image from "next/image";
+
+import Overlay from "./Overlay";
+import { useAuth } from "@clerk/nextjs";
+import Footer from "./Footer";
+
+import { Skeleton } from "@/components/ui/skeleton";
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  authorId: string;
+  authorName: string;
+  createdAt: number;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+const BoardCard = ({
+  id,
+  title,
+  imageUrl,
+  authorId,
+  authorName,
+  createdAt,
+  orgId,
+  isFavorite,
+}: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = userId === authorId ? "You" : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, {
+    addSuffix: true,
+  });
+
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+        <div className="relative flex-1 bg-amber50">
+          <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Overlay />
+        </div>
+        <Footer
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default BoardCard;
+
+BoardCard.Skeleton = function BoardCardSkeleton() {
+  return (
+    <div className="aspect-[100/127] rounded-lg overflow-hidden">
+      <Skeleton className="h-full w-full" />
+    </div>
+  );
+};
